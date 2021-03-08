250b99aa2bc147d8f554219b4cb18a112e8e25ac
buidlman <mr.tolstoy@protonmail.ch>
2021-03-08 17:34:43 +0300

    add failed test for register bridge

--- tests/rewards/test_reward_by_bridge.py
+++ tests/rewards/test_reward_by_bridge.py
@@ -6,3 +6,9 @@ def test_register_bridge(deployer, block_reward, bridge):
     block_reward.setBridgeAllowed([bridge], {"from":deployer})
 
     assert block_reward.bridgesAllowed(0) == bridge
+
+
+def test_register_bridge_failed(deployer, block_reward, bridge):
+
+    with brownie.reverts():
+         block_reward.setBridgeAllowed([bridge], {"from":bridge})
